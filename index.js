6e71849ce940b19492c0d625a4bdab899b9597e1
Donghyeok Shin <smdh1010@gmail.com>
2024-09-23 20:16:50 +0900

    feature: add mockData.json

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ require('dotenv').config();
 const Fastify = require('fastify');
 const axios = require('axios');
 const fastify = Fastify({ logger: true });
+const mockData = require('./mockData.json');
 
-// 경로 검색 + 실시간 버스 도착정보 조회
+const USE_MOCK = process.env.USE_MOCK === 'true';
+
+// 경로 검색
 fastify.get('/odsay/dirs', async(request, reply) => {
     const { SX, SY, EX, EY } = request.query;
     const url = `https://api.odsay.com/v1/api/searchPubTransPathT?SX=${SX}&SY=${SY}&EX=${EX}&EY=${EY}&apiKey=${process.env.ODSAY_API_KEY}`;
 
-    try {
-        const response = await axios.get(url);
-        return response.data;
-    } catch (error) {
-        fastify.log.error(error);
-        reply.status(error.status).send({ error });
+    if (USE_MOCK) {
+        return mockData.searchPubTransPathT;
+    } else {
+        try {
+            const response = await axios.get(url);
+            return response.data;
+        } catch (error) {
+            fastify.log.error(error);
+            reply.status(error.status).send({ error });
+        }
     }
 });
 
@@ -27,17 +34,21 @@ fastify.get('/odsay/bus/realtime', async(request, reply) => {
         url = `https://api.odsay.com/v1/api/realtimeStation?stationID=${stationID}&apiKey=${process.env.ODSAY_API_KEY}`;
     }
     
-    try {
-        const response = await axios.get(url);
-        return response.data;
-    } catch (error) {
-        fastify.log.error(error);
-        reply.status(error.status).send({ error });
+    if (USE_MOCK) {
+        return mockData.realtimeStation;
+    } else {
+        try {
+            const response = await axios.get(url);
+            return response.data;
+        } catch (error) {
+            fastify.log.error(error);
+            reply.status(error.status).send({ error });
+        }
     }
 });
 
 // 노선 그래픽 데이터 검색
-fastify.get('/odsay/loadlane', async(request, reply) => {
+fastify.get('/odsay/loadLane', async(request, reply) => {
     const { mapObj } = request.query;
     let mapObjList;
     if (mapObj.includes('@')) {
@@ -46,19 +57,23 @@ fastify.get('/odsay/loadlane', async(request, reply) => {
         mapObjList = [mapObj];
     }
 
-    try {
-        const responseDataList = await Promise.all(
-            mapObjList.map(async (mapObjItem) => {
-                const url = `https://api.odsay.com/v1/api/loadLane?mapObject=0:0@${mapObjItem}&apiKey=${process.env.ODSAY_API_KEY}`;
-                const response = await axios.get(url);
-                return response.data;
-            })
-        );
-
-        return responseDataList;
-    } catch (error) {
-        fastify.log.error(error);
-        return reply.status(error.response ? error.response.status : 500).send({ error });
+    if (USE_MOCK) {
+        return mockData.loadLane;
+    } else {
+        try {
+            const responseDataList = await Promise.all(
+                mapObjList.map(async (mapObjItem) => {
+                    const url = `https://api.odsay.com/v1/api/loadLane?mapObject=0:0@${mapObjItem}&apiKey=${process.env.ODSAY_API_KEY}`;
+                    const response = await axios.get(url);
+                    return response.data;
+                })
+            );
+    
+            return responseDataList;
+        } catch (error) {
+            fastify.log.error(error);
+            return reply.status(error.response ? error.response.status : 500).send({ error });
+        }
     }
 });
 
